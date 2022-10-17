741c6515ffdd4e8ac25e2ddb11007f67bc0a591c
cmatos <carlos.matos@capgemini.com>
2022-10-17 13:42:40 -0300

    ...

--- assets/js/script.form.js
+++ assets/js/script.form.js
@@ -1,22 +1,5 @@
 (() => {
 
-    let currFieldSetIndex = 0;
-
-    const steps = Array.from(document.querySelectorAll(".step .circle"));
-    const form = document.getElementById("signup-form");
-    const fieldsetArr = Array.from(form.getElementsByTagName("fieldset"));
-    const lastFieldSetIndex = fieldsetArr.length - 1;
-
-    window.addEventListener('keydown', function (e) {
-        if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
-            if (e.target.nodeName == 'INPUT') {
-                e.preventDefault();
-                requestNextBtnOrSubmit(currFieldSetIndex);
-                return false;
-            }
-        }
-    }, true);
-
     const forward = (index) => {
         currFieldSetIndex = index + 1;
         form.style.marginLeft = (currFieldSetIndex * -100) + '%';
@@ -40,13 +23,18 @@
         }
     };
 
-    const selectInputField = (index) => {
-        let input = fieldsetArr[index].querySelector("input:required");
+    const selectInputField = (input, timeout) => {
         if (!!input) {
             setTimeout(() => {
                 input.focus();
-            }, 400);
+                input.select();
+            }, timeout || 20);
         }
+    };
+
+    const selectFirstInputField = (index) => {
+        let input = fieldsetArr[index].querySelector("input:required");
+        selectInputField(input, 400);
     };
 
     const invokeClick = (index, query) => {
@@ -59,21 +47,66 @@
         return false;
     };
 
+    const isValidInputField = (input) => {
+        return !!!input.value || !!!input.value.length;
+    };
+
+    const isAllRequiredInputFieldFilled = (index) => {
+        const fieldset = fieldsetArr[index];
+        const fields = fieldset.querySelectorAll("input:required");
+        for (let field of fields) {
+            if (isValidInputField(field)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    const selectNonValidFieldRequired = (index) => {
+        const fieldset = fieldsetArr[index];
+        const fields = fieldset.querySelectorAll("input:required");
+        for (let field of fields) {
+            if (isValidInputField(field)) {
+                selectInputField(field);
+                break;
+            }
+        }
+    };
+
     const requestNextBtnOrSubmit = (index) => {
         return invokeClick(index, "input.btn-next") ||
             invokeClick(index, "input[type='submit']");
     };
+
+    window.addEventListener('keydown', function (e) {
+        if (e.keyIdentifier == 'U+000A' || e.keyIdentifier == 'Enter' || e.keyCode == 13) {
+            if (e.target.nodeName == 'INPUT') {
+                e.preventDefault();
+                if (isAllRequiredInputFieldFilled(currFieldSetIndex)) {
+                    requestNextBtnOrSubmit(currFieldSetIndex);
+                } else {
+                    selectNonValidFieldRequired(currFieldSetIndex);
+                }
+                return false;
+            }
+        }
+    }, true);
+
+    let currFieldSetIndex = 0;
+    const steps = Array.from(document.querySelectorAll(".step .circle"));
+    const form = document.getElementById("signup-form");
+    const fieldsetArr = Array.from(form.getElementsByTagName("fieldset"));
 
     for (let i in fieldsetArr) {
         const index = parseInt(i);
         const fieldset = fieldsetArr[index];
         setupBtn(fieldset, "input.btn-next", () => {
             forward(index);
-            selectInputField(index + 1);
+            selectFirstInputField(index + 1);
         });
         setupBtn(fieldset, "input.btn-prev", () => {
             backward(index);
-            selectInputField(index - 1);
+            selectFirstInputField(index - 1);
         });
     }
 })();
